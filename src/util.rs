1ab6884164bc051bfbb6d1e0824d3ce91fc62ae5
Isaac Sikkema <isaacsikkema@gmail.com>
2023-05-13 20:19:53 -0400

    Remove util.rs

--- src/util.rs
+++
@@ -1,7 +0,0 @@
-pub fn flatten_arrays<T: Clone, const N: usize, const M: usize>(arrays: [[T; M]; N]) -> [T; N * M] {
-    if let Ok(flat_array) = arrays.concat().try_into() {
-        flat_array
-    } else {
-        unreachable!()
-    }
-}
