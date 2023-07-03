bc5534f474c7bb214f900b6b342dc7ba7d3b7950
Isaac Sikkema <isaacsikkema@gmail.com>
2023-07-02 23:06:06 -0400

    Add planning comments

--- src/qubit_system.rs
+++ src/qubit_system.rs
@@ -1,4 +1,8 @@
-use crate::{braket::Ket, qubit::Qubit, tensor_mul::TensorMul, basis::Basis};
+use rand::{distributions::WeightedIndex, prelude::Distribution, thread_rng};
+
+use crate::{
+    basis::Basis, braket::Ket, qubit::Qubit, tensor_mul::TensorMul, transposable::Transposable,
+};
 
 #[derive(Debug)]
 pub struct QubitSystem<const N: usize> {
@@ -16,8 +20,35 @@ impl<const N: usize> QubitSystem<N> {
         todo!()
     }
 
-    pub fn measure(&mut self, basis: Basis<N>) -> Ket<f64, N> {
+    pub fn get_probability_amplitudes(&self, basis: Basis<N>) -> Ket<f64, N> {
+        basis.into_bra().T() * self.state.clone()
+    }
+
+    pub fn get_probabilities(&self, basis: Basis<N>) -> Ket<f64, N> {
+        self.get_probability_amplitudes(basis).map(|n| n.powi(2))
+    }
+
+    /*
+    Measure only 1 qubit at a time.
+    Sum up probabilities for 0 vs 1, choose, then collapse inner state in half.
+    Only elements where the qubit was {result} remain.
+    How to normalize amplitudes afterwards?
+     */
+    pub fn measure(
+        self,
+        qubit: usize, /* how to specify qbit to measure??? */
+        basis: Basis<2>,
+    ) -> (QubitSystem<{ N / 2 }>, Ket<f64, 2>) {
         todo!()
+        // let states = basis.as_bra().as_array().clone();
+        // let probabilities = self.get_probabilities(basis);
+
+        // let dist = WeightedIndex::new(probabilities.as_array()).unwrap();
+        // let idx = dist.sample(&mut thread_rng());
+
+        // self.state = states[idx].clone();
+
+        // &self.state
     }
 
     pub(crate) fn from_ket(ket: Ket<f64, N>) -> Self {
