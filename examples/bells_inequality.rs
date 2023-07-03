1b4da663d6e094a1d94a9ce55d0832fdbddf1554
Isaac Sikkema <isaacsikkema@gmail.com>
2023-07-02 23:05:50 -0400

    Add measurement direction

--- examples/bells_inequality.rs
+++ examples/bells_inequality.rs
@@ -2,7 +2,25 @@
 
 use std::{env, num::ParseIntError};
 
-use qubit::{qgates, qubit::Qubit, qubit_system::QubitSystem};
+use qubit::{qgates, qubit::Qubit, qubit_system::QubitSystem, basis::Basis};
+use rand::{distributions::Standard, prelude::Distribution, random, thread_rng, Rng};
+
+#[derive(Debug)]
+enum MeasureDirection {
+    Deg0,
+    Deg120,
+    Deg240,
+}
+
+impl Distribution<MeasureDirection> for Standard {
+    fn sample<R: rand::Rng + ?Sized>(&self, rng: &mut R) -> MeasureDirection {
+        match rng.gen_range(0..=2) {
+            0 => MeasureDirection::Deg0,
+            1 => MeasureDirection::Deg120,
+            _ => MeasureDirection::Deg240,
+        }
+    }
+}
 
 fn main() -> Result<(), ParseIntError> {
     let num_qubits: u32 = match env::args().nth(1) {
@@ -10,7 +28,8 @@ fn main() -> Result<(), ParseIntError> {
         None => 100_000,
     };
 
-    // for _ in 0..num_qubits {
+    let mut rng = thread_rng();
+
     let qbs: QubitSystem<2> = Qubit::deg45().into();
     println!("{qbs:?}");
 
@@ -20,7 +39,17 @@ fn main() -> Result<(), ParseIntError> {
     let qbs = qgates::cnot(qbs);
 
     println!("{qbs:?}");
-    // }
+
+    let md: MeasureDirection = rng.gen();
+
+    println!("{md:?}");
+
+    // We need to measure one at a time
+    let measure_basis = match md {
+        MeasureDirection::Deg0 => todo!(),
+        MeasureDirection::Deg120 => todo!(),
+        MeasureDirection::Deg240 => todo!(),
+    };
 
     Ok(())
 }
